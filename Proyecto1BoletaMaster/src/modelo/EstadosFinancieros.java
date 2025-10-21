36f73cc441e4a4d805bd3e82b70a13aa2cbe5828
laura-sanchezm <l.sanchezm2@uniandes.edu.co>
2025-10-20 23:01:45 -0500

    estados financieros

---
+++ Proyecto1BoletaMaster/src/modelo/EstadosFinancieros.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.util.*;
+
+import Pago.*;
+
+import java.time.*;
+
+public class EstadosFinancieros {
+	
+	private List<Pago> transacciones;
+	
+	public void registrarTransaccion() {
+	// TO DO	
+	}
+	
+	public void anularTransaccion() {
+		// TO DO	
+	}
+	
+	public int totalGanancias(LocalDate fechaMin, LocalDate fechaMax, int idE, int idO) {
+		// TO DO	
+		return 0;
+	}
+	
+	public int ingresoBaseOrganizador(int idO, LocalDate fechaMin, LocalDate fechaMax, int idE, int idL) {
+		// TO DO
+		return 0;
+	}
+	
+	public int porcentajeVenta(int idE, int idL) {
+		// TO DO
+		return 0;
+	}
+
+}
